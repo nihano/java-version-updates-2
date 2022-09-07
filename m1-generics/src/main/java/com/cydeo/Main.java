8cd0642a56a7f074c9713cc24aef1059e0f2f6c0
nihano <nioztekin@gmail.com>
2022-09-08 00:04:47 +0300

    project set up, gitignore created and m1-generic module created.

---
+++ m1-generics/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,18 @@
+package com.cydeo;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList items = new ArrayList<>();
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+
+    }
+
+
+}
