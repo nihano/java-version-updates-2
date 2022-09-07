27a0a5bd2f301ddcd3e0f7fd32f97d5b26458d0e
nihano <nioztekin@gmail.com>
2022-09-08 00:48:05 +0300

    updated classess

--- m1-generics/src/main/java/com/cydeo/Player.java
+++ m1-generics/src/main/java/com/cydeo/Player.java
@@ -1,6 +1,14 @@
 package com.cydeo;
 
-public class Player {
+public abstract class Player {
 
+    private String name;
 
+    public Player(String name){
+        this.name=name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
