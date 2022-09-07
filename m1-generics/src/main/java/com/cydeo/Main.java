b9990c3894cea4e1dbf8df4bd35108a53edbe089
nihano <nioztekin@gmail.com>
2022-09-08 00:32:45 +0300

    main class updated

--- m1-generics/src/main/java/com/cydeo/Main.java
+++ m1-generics/src/main/java/com/cydeo/Main.java
@@ -6,12 +6,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList items = new ArrayList<>();
+        ArrayList<Integer> items = new ArrayList<Integer>(); //Arraylist only accepts objects belongs to integer class, this means generics. It will not allow ading other than Integer
+        //problem was class and interface structure in Java did not have any type protection that's why generics introduced.
         items.add(1);
         items.add(2);
         items.add(3);
-        items.add(4);
+        //items.add("Apple");
 
+        printDoubled(items);
+
+    }
+
+    public static void printDoubled(ArrayList<Integer> items){
+
+        for (Integer i: items ){
+            System.out.println(i*2);
+        }
     }
 
 
