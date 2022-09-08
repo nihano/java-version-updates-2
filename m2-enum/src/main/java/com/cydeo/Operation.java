5918c7452a9c2dfa8b0d0d0b0b85648867012e87
nihano <nioztekin@gmail.com>
2022-09-08 16:37:43 +0300

    Operation.java updated

--- m2-enum/src/main/java/com/cydeo/Operation.java
+++ m2-enum/src/main/java/com/cydeo/Operation.java
@@ -2,5 +2,11 @@ package com.cydeo;
 
 public enum Operation {
 
-    PLUS, MINUS, MULTIPLY, DIVIDE
+    PLUS, MINUS, MULTIPLY, DIVIDE; // each constant is object of the Operation
+
+    Operation() { //this constructor cannot be public, we cannot call this constructor, it is private by default. values already constant we cannot assign new values?
+        System.out.println("Constructor");  // this prints constructor 4 times? I did not get it.
+    }
+
+
 }
