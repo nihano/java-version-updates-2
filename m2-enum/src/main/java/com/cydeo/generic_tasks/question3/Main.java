d275a9302a6912327f003845ea21be1504f83896
nihano <nioztekin@gmail.com>
2022-09-08 17:12:39 +0300

    Initial commit

---
+++ m2-enum/src/main/java/com/cydeo/generic_tasks/question3/Main.java
@@ -0,0 +1,15 @@
+package com.cydeo.generic_tasks.question3;
+
+public class Main {
+}
+
+/*
+
+Will the following class compile? If not, why?
+public final class Algorithm{
+    public static <T> T max(T x, T y){
+
+    return x > y ? x : y;
+    }
+ }
+ */
