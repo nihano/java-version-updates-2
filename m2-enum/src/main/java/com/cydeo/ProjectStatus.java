e67cac5c214faa6aa725781754aa77c8fc5b49fd
nihano <nioztekin@gmail.com>
2022-09-08 16:37:53 +0300

    ProjectStatus.java updated

--- m2-enum/src/main/java/com/cydeo/ProjectStatus.java
+++ m2-enum/src/main/java/com/cydeo/ProjectStatus.java
@@ -1,4 +1,15 @@
 package com.cydeo;
 
 public enum ProjectStatus {
+
+    OPEN, CLOSED, PROGRESS("IN PROGRESS");
+
+    String status;
+
+    ProjectStatus(String status) {
+        this.status = status;
+    }
+
+    ProjectStatus() { //default constructor for initializing OPEN and CLOSED without passing value
+    }
 }
