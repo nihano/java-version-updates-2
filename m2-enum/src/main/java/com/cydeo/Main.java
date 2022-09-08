9dcfc1c82fd58caec3eae771c4fbd6e25a9367e0
nihano <nioztekin@gmail.com>
2022-09-08 16:38:08 +0300

    Main.java updated

--- m2-enum/src/main/java/com/cydeo/Main.java
+++ m2-enum/src/main/java/com/cydeo/Main.java
@@ -14,9 +14,57 @@ public class Main {
         System.out.println("How to access all the constants?");
         Currency[] currencies = Currency.values(); //values() method returns array of the constants
 
-        for (Currency currency : currencies){
+        for (Currency currency : currencies) {
             System.out.println(currency);
         }
 
+        System.out.println("how to use switch cases with enums");
+
+        Currency myCurrency = Currency.QUARTER;
+        switch (myCurrency) {
+            case PENNY:
+                System.out.println("it is 1 cent");
+                break;
+            case NICKEL:
+                System.out.println("it is 5 cents");
+                break;
+            case DIME:
+                System.out.println("it is 10 cents");
+                break;
+            case QUARTER:
+                System.out.println("it is 25 cents");
+                break;
+        }
+
+        System.out.println("Operation task");
+        calculate(3, 5, Operation.PLUS);
+
     }
+
+    private static void calculate(double x, double y, Operation operation) {
+
+        switch (operation) {
+            case PLUS:
+                System.out.println(x+y);
+                break;
+            case MINUS:
+                System.out.println(x-y);
+                break;
+            case MULTIPLY:
+                System.out.println(x*y);
+                break;
+            case DIVIDE:
+                System.out.println(x/y);
+                break;
+        }
+
+
+    }
+
 }
+
+/*
+Task: create a method for Math operations
+      create a method called 'calculate' which accepts 3 parameters and prints the result
+      based on the selected operator in switch-case  declared inside the method
+ */
