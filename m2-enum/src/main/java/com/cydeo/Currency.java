29b8982a965a7c0ced4381de80751f1eb75e38c4
nihano <nioztekin@gmail.com>
2022-09-08 16:38:19 +0300

    Currency.java updated

--- m2-enum/src/main/java/com/cydeo/Currency.java
+++ m2-enum/src/main/java/com/cydeo/Currency.java
@@ -3,6 +3,16 @@ package com.cydeo;
 import java.lang.Enum;
 public enum Currency {
 
-    PENNY, NICKEL, DIME, QUARTER //these are objects of currency class
+    PENNY(1), NICKEL(5), DIME(10), QUARTER(25);//these are objects of currency class
+    //this is how we assign values to constants
 
+    private int value;
+
+    Currency(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
