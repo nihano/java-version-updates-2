1c1d129490fc3315a01c0a5de0faa3a3b31e5508
nihano <nioztekin@gmail.com>
2022-09-10 00:39:19 +0300

    Updated

--- m3-lambda/src/main/java/com/cydeo/ApplePredicate.java
+++ m3-lambda/src/main/java/com/cydeo/ApplePredicate.java
@@ -1,6 +1,11 @@
 package com.cydeo;
 
+import lombok.ToString;
+
 @FunctionalInterface //to make sure it has only one abstract method
+
+
 public interface ApplePredicate {
     boolean test(Apple apple);
+
 }
--- m3-lambda/src/main/java/com/cydeo/AppleTest.java
+++ m3-lambda/src/main/java/com/cydeo/AppleTest.java
@@ -36,6 +36,7 @@ public class AppleTest {
 
         return result;
     }
+
 }
 
 
