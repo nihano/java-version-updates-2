68978ad749ee5f9a1f7b3c2c8d5098df98d52f47
nihano <nioztekin@gmail.com>
2022-09-11 15:56:37 +0300

    Comparator Interface, compareTo method

---
+++ m5-comparator_interface/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,34 @@
+package com.cydeo;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer>  list = Arrays.asList(10,0,15,5,20);
+        System.out.println(list);
+
+        //Ascending
+        Collections.sort(list);
+        System.out.println(list);
+
+        Collections.sort(list, new MyComparator());
+        System.out.println(list);
+
+        Collections.sort(list, ((o1, o2) -> (o1>o2) ? -1 : (o1<o2) ? 1 : 0));// how did it work?
+
+        //Ascending order
+        list.sort((o1,o2) -> o1.compareTo(o2));
+        System.out.println(list);
+
+        //Descending order
+        list.sort((o2, o1) -> o1.compareTo(o2));
+        System.out.println(list);
+
+
+
+    }
+}
