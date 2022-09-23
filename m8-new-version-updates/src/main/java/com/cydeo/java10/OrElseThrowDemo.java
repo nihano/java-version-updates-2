82464b9c1e2c0cf60a2e81a914640c1443ae380a
nihano <nioztekin@gmail.com>
2022-09-24 02:19:02 +0300

    Java10 version updates

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java10/OrElseThrowDemo.java
@@ -0,0 +1,29 @@
+package com.cydeo.java10;
+
+import java.util.Optional;
+
+public class OrElseThrowDemo {
+    public static void main(String[] args) {
+
+        //replacement of the get() method and isPresent()
+
+        Optional<String> str = Optional.empty();
+
+       //System.out.println(str.get()); //will throw exception
+
+        /*
+        if (str.isPresent()){
+            System.out.println(str.get());
+        }else {
+            //handle the error
+        }
+
+         */
+
+        System.out.println(str.orElseThrow());
+
+
+
+
+    }
+}
